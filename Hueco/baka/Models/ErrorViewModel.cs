f5163db2eea4fad1fff70032dc74f9882f7ebe7e
MasterOfFun <francoisjeannin9@gmail.com>
2024-04-09 00:34:52 +0200

    update

--- Hueco/baka/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace baka.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
